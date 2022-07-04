d3aec82e64610fe43c92e45eb07b3693ab9bd01d
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-07-03 18:18:46 -0700

    Select the button and unordered list (will be needed when the event listener is later added), create a factory function and declare a vehicle object with three properties, and add three parameters that will be used as the property values

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+const button = document.querySelector("button");
+// select the unordered list
+const repairList = document.querySelector(".need-repair");
 
+// create a factory function and add three parameters
+const createVehicle = function (type, numWheel, color) {
+  // declare an object called vehicle with three properties and use the parameters as values
+  const vehicle = {
+    type: type,
+    numWheel: numWheel,
+    color: color,
+  };
+  return vehicle;
+};
